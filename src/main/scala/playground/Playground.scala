370ecd1f4be4c38959fdcc87c37572d47fe24587
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2018-11-12 22:53:17 +0000

    everything caught on camera

---
+++ src/main/scala/playground/Playground.scala
@@ -0,0 +1,16 @@
+package playground
+
+import akka.actor.ActorSystem
+
+/**
+  * A simple Scala application. I recommend you fiddle with the code and try your own here.
+  * Have fun!
+  *
+  * Daniel for Rock the JVM
+  */
+object Playground extends App {
+
+  val system = ActorSystem("Playground")
+
+  // your code here
+}
