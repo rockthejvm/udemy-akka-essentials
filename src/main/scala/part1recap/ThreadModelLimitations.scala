13a8a8c2d55b715bfffbe9ec201a3bade69f77e2
Petr Osipov <peterosipov@cloud.upwork.com>
2021-01-09 20:44:18 +0500

    correcting overflow & adding missed 1M to the sum

--- src/main/scala/part1recap/ThreadModelLimitations.scala
+++ src/main/scala/part1recap/ThreadModelLimitations.scala
@@ -87,12 +87,12 @@ object ThreadModelLimitations extends App {
   import scala.concurrent.ExecutionContext.Implicits.global
 
   val futures = (0 to 9)
-    .map(i => 100000 * i until 100000 * (i + 1)) // 0 - 99999, 100000 - 199999, 200000 - 299999 etc
+    .map(i => BigInt(100000) * i until BigInt(100000) * (i + 1)) // 0 - 99999, 100000 - 199999, 200000 - 299999 etc
     .map(range => Future {
       if (range.contains(546735)) throw new RuntimeException("invalid number")
       range.sum
     })
 
-  val sumFuture = Future.reduceLeft(futures)(_ + _) // Future with the sum of all the numbers
+  val sumFuture = Future.foldLeft(futures)(BigInt(1000000))(_ + _) // Future with the sum of all the numbers
   sumFuture.onComplete(println)
 }
